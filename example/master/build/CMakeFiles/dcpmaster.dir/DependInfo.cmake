2656fc4bcedea84ebdd2828de2bfe6950b9bdbab
root <root@beaglebone.localdomain>
2020-04-10 12:32:07 +0000

    initial commit

---
+++ example/master/build/CMakeFiles/dcpmaster.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/2020-map-hybridcosim/DCPLib/example/master/src/Main.cpp" "/home/2020-map-hybridcosim/DCPLib/example/master/build/CMakeFiles/dcpmaster.dir/src/Main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ASIO_STANDALONE"
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/usr/local/include/DCPLib/ethernet"
+  "/usr/local/include"
+  "/usr/local/include/DCPLib/core"
+  "/usr/local/include/DCPLib/master"
+  "/usr/local/include/DCPLib/xml"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ example/master/build/CMakeFiles/dcpmaster.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dcpmaster.dir/src/Main.cpp.o"
+  "dcpmaster.pdb"
+  "dcpmaster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dcpmaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
