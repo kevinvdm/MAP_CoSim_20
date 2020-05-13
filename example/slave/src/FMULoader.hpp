6af26bc0a7adf042fc87c7381cdda1d2a263db0a
Kevin Van de Mieroop <kevinvdm@msn.com>
2020-05-13 20:50:24 +0200

    changed data types

--- example/slave/src/FMULoader.hpp
+++ example/slave/src/FMULoader.hpp
@@ -1,5 +1,4 @@
 #include <fmi4cpp/fmi4cpp.hpp>
-
 #include <iostream>
 #include <string>
 
@@ -7,18 +6,18 @@ using namespace fmi4cpp;
 
 class FMULoader {
 public:
-
     FMULoader() {
-        fmi2::fmu fmu("ExternalSim_Test.fmu");
 
-        auto cs_fmu = fmu.as_cs_fmu();
+        fmi2::fmu fmu(fmu_path);
+        cs_fmu = fmu.as_cs_fmu();
+
         auto md = cs_fmu->get_model_description();
 
         auto var = md->model_variables->getByValueReference(1).as_real();
         
         std::cout << "Name=" << var.name() << ", start=" << var.start().value_or(0) << std::endl;
 
-        auto slave = cs_fmu->new_instance();
+        slave = cs_fmu->new_instance();
 
         std::cout << "model_identifier=" << slave->get_model_description()->model_identifier << std::endl;
 
@@ -41,7 +40,6 @@ public:
         }
 
         std::cout << "FMU '" << fmu.model_name() << "' terminated with success: " << (slave->terminate() == 1 ? "true" : "false") << std::endl;
-        // std::cout << "FMU '" << fmu.model_name() << "' terminated with success: " << (slave2->terminate() == 1 ? "true" : "false") << std::endl;
 
     }
 
@@ -51,6 +49,11 @@ public:
 
 
 private:
+
+
+    const std::string fmu_path;;
+    std::unique_ptr<fmi4cpp::fmi2::cs_fmu> cs_fmu;
+    std::unique_ptr<fmi2::cs_slave> slave;
 
     const double stop = 10;
     const double stepSize = 1E-3;
