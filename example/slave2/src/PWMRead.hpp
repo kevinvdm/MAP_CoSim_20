7b7d79d8844e3aaaf9d04dd40afe7f3d3c7b43a8
Kevin Van de Mieroop <kevinvdm@msn.com>
2020-05-19 00:17:51 +0200

    added pwm write to pwmread.hpp

--- example/slave2/src/PWMRead.hpp
+++ example/slave2/src/PWMRead.hpp
@@ -8,10 +8,13 @@
 
 //! The pin for CAP input.
 #define P_IN P9_42
+//! The pin for PWM output.
+#define P_OUT P9_21
 
 class PWMRead {
 public:
     PWMRead() {
+
         //configure input pin
         if (pruio_cap_config(Io, P_IN, 2.)) { 
           printf("failed setting input @P_IN (%s)\n", Io->Errr);
@@ -37,7 +40,16 @@ public:
         if (pruio_cap_Value(Io, P_IN, &f, &d)) {
           printf("failed reading input @P_IN (%s)\n", Io->Errr);
         }
+        printf("Duty cycle: %10f", d);
         return d;
+    }
+
+    //set new output
+    float_t writePWM(float_t fo){
+      if (pruio_pwm_setValue(Io, P_OUT, fo, 0.5)) {
+        printf("failed setting output @P_OUT (%s)\n", Io->Errr);
+      }
+      printf("Set frequency on P9_21: %10f", fo);
     }
 
 private:
@@ -48,4 +60,5 @@ private:
     //calculated frequency f and duty cycle d
     float_t f;
     float_t d;
+
 };
