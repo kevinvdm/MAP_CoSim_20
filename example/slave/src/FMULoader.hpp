896192565f090f131726db5aaaf1ffac80378cb3
Kevin Van de Mieroop <kevinvdm@msn.com>
2020-07-29 00:04:21 +0200

    wltp initial results with old pid

--- example/slave/src/FMULoader.hpp
+++ example/slave/src/FMULoader.hpp
@@ -66,7 +66,7 @@ public:
 private:
 
 
-    const std::string fmu_path = "./Controller_Seperate2_newPID_Step.fmu";
+    const std::string fmu_path = "./Controller_Seperate2_newPID_WLTP.fmu";
     std::unique_ptr<fmi4cpp::fmi2::cs_fmu> cs_fmu;
     std::unique_ptr<fmi4cpp::fmi2::cs_slave> slave;
     std::shared_ptr<const fmi4cpp::fmi2::cs_model_description> md;
